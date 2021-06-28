afe2f0dfcd40f1e6e7fb12e4c3e5b07ea8bb7b1a
Aysegulbetul <cakirayse2015@gmail.com>
2021-06-28 19:38:47 +0200

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing Hello feature");
+        System.out.println("local repo is already published to Gitup");
+        System.out.println("Doing more work");
     }
 }
